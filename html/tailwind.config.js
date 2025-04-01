f9de29b0ef7493692598331c15782fa9132210e0
ryd10227 <ryd10227@gmail.com>
2025-04-01 10:18:59 +0900

    html first commit

---
+++ html/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        carrot: "#e67e22",
+        danger: "#e3342f",
+        main: "#00d8d6",
+      },
+    },
+  },
+};
